a4fcc8f4939ff6d96b1121d37d7afaf56d3c1fe1
a2eas <ahmedpko095@gmail.com>
2025-12-26 11:02:47 +0200

    full noise file

--- noise_function.py
+++ noise_function.py
@@ -1,5 +1,17 @@
 import cv2
-device = "cuda" if torch.cuda.is_avaliable() else 'cpu'
-def noise(og_image,schedule_type,time_step,epslion,T,noise_mean=0,noise_variance=1):
-    og_image = cv2.resize(og_image,(64,64)) /255.0
-    og_image = cv2.cvtColor(og_image,cv2.COLOR_BGR2RGB)
+import torch
+import math
+
+
+device = "cuda" if torch.cuda.is_available() else 'cpu'
+def reverse_noise(og_image,t,eps_hat,alphas):
+    sqrt_alpha = torch.sqrt(alphas[t])
+    sqrt_alpha_minus_one = torch.sqrt(1-alphas[t])
+    x_hat = (og_image- sqrt_alpha_minus_one * eps_hat) / sqrt_alpha
+    return x_hat
+def noise_foward(image,t,alphas):
+    eps = torch.randn_like(image)
+    sqrt_alpahs = torch.sqrt(alphas[t])
+    sqrt_alpahs_hat = torch.sqrt(1-alphas[t])
+    x_t = sqrt_alpahs*image + sqrt_alpahs_hat*eps
+    return x_t,eps
