ff66368ada68aebe2a97630015a660a3c7e0311c
a2eas <ahmedpko095@gmail.com>
2026-01-07 14:57:29 +0200

    noise finally done

--- noise_function.py
+++ noise_function.py
@@ -1,20 +1,35 @@
 import cv2
 import torch
 import math
-
+from noise_scheduler import Noise_Scheduler
+import matplotlib.pyplot as plt
 
 device = "cuda" if torch.cuda.is_available() else 'cpu' # setting the device to gpu if avaliable 
-def reverse_noise(og_image,t,eps_hat,alphas):
+def reverse_noise(noise_image,t,eps_hat,alphas,alphas_hat):
     #t = timesteps 
     #eps nosise added
     #alpahs how much of the image is perseved at timestep t
-    sqrt_alpha = torch.sqrt(alphas[t]) # squarte root of alphas at time t
-    sqrt_alpha_minus_one = torch.sqrt(1-alphas[t]) # minus one also square root
-    x_hat = (og_image- sqrt_alpha_minus_one * eps_hat) / sqrt_alpha # formual for getting the og image before noise
+    sqrt_alpha = torch.sqrt(alphas) # squarte root of alphas at time t
+    sqrt_alpha_minus_one = torch.sqrt(alphas_hat) # minus one also square root
+    x_hat = (noise_image- sqrt_alpha_minus_one * eps_hat) / sqrt_alpha # formual for getting the og image before noise
     return x_hat
-def noise_foward(image,t,alphas):
+def noise_foward(image,t,alphas,alphas_hat):
+    image = torch.tensor(image)
+    image = image.float() /255.0
     eps = torch.randn_like(image) #genrating random noise in the same size as the image
-    sqrt_alpahs = torch.sqrt(alphas[t])
-    sqrt_alpahs_hat = torch.sqrt(1-alphas[t])
+    sqrt_alpahs = torch.sqrt(alphas_hat[t])
+    sqrt_alpahs_hat = torch.sqrt(1.0 - alphas_hat[t])
     x_t = sqrt_alpahs*image + sqrt_alpahs_hat*eps # formual for adding noise to the image
     return x_t,eps
+
+if __name__ == "__main__":
+    img = cv2.imread(r'Screenshot 2025-12-24 223016.png')
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    b,a,a_bar = Noise_Scheduler(t=1000)
+    x_t , eps = noise_foward(img,401,a,a_bar)
+    print('alphas_bar[t]', a_bar[999].item())
+
+    plt.imshow(x_t.clamp(-2,2), cmap="gray")
+    plt.axis("off")
+    plt.show()
+
--- noise_scheduler.py
+++ noise_scheduler.py
@@ -3,8 +3,8 @@ import torch
 
 
 def Noise_Scheduler(t = 1000,beta_start =1e-4,beta_end = 0.02,device='cpu'):
-    beats = torch.linspace(beta_start,beta_end,t,device=device)
-    alphas = 1.0 - beats
+    betas = torch.linspace(beta_start,beta_end,t,device=device)
+    alphas = 1.0 - betas
     alphas_bar = torch.cumprod(alphas,dim=0)
 
-    return beats,alphas,alphas_bar
+    return betas,alphas,alphas_bar
